f5d0a2fefa79fff74926d496610a16b2704d297e
goodminton <perished_hgp@qq.com>
2025-10-30 10:43:24 +0800

    day5内容

--- week1/day5.py
+++ week1/day5.py
@@ -33,3 +33,51 @@ print(f"全一矩阵：\n{one_arr}")
 # 创建一个从 0 到 9 的一维数组
 range_arr = np.arange(10)
 print(f"范围数组：{range_arr}")
+
+print("\n --- 4. 索引、切片与向量化 ---")
+print("\n --- 索引与切片 ---")
+# 索引与切片
+arr = np.arange(10) # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(f"第五个元素（索引为4）：{arr[4]}")
+
+matrix = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+# 获取单个元素：[行，列]
+print(f"获取第2行第3列元素（索引1，2）：{matrix[1, 2]}")
+
+# 获取一整行
+print(f"获取第 2 整行（索引1）：{matrix[1]}")
+
+# 获取一整列
+print(f"获取第 3 整列（索引2）：{matrix[:, 2]}")
+
+# 向量化操作
+data = np.arange(5) # [0, 1, 2, 3, 4]
+
+# 告别 for 循环
+print(f"\n ---向量化操作 ---")
+print(f"原数组:{data}")
+print(f"所有元素乘以 2 :{data * 2}")
+print(f"所有元素加上 10 :{data + 10}")
+
+# 数组间的运算
+data2 = np.ones(5)
+print(f"另一个数组为:{data2}")
+print(f"两个数组相加得到: {data + data2}")
+
+# 通用函数 (Universal Functions, ufuncs)
+print(f"对所有元素求正弦: {np.sin(data)}")
+
+# 今日任务
+print("\n--- 任务一：创建随机数组 ---")
+arr_random = np.random.rand(5, 5)
+print(f"创建一个 5x5 的随机数组:\n{arr_random}")
+# 所有大于 0.5 的数变为 1，所有小于等于 0.5 的数变为 0。
+
+print(f"\n--- 任务二：向量化条件赋值 ---")
+arr_random[arr_random > 0.5] = 1
+arr_random[arr_random <= 0.5] = 0
+print(f"赋值之后的数组：\n{arr_random}")
+
+print("\n--- 任务三：提取子集 ---")
+print(f"提取第二整行: {arr_random[1]}")
+print(f"提取第三整列: {arr_random[:, 2]}")
